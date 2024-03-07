86c42b60c3b19b66f080bd92c68a41b53ef512b0
eliampimentel <eliampimentel@prodominicana.gob.do>
2024-03-07 09:12:27 -0400

    rutas dinamicas (transparencia)

--- src/components/transparencia/menu/menu-docs.tsx
+++ src/components/transparencia/menu/menu-docs.tsx
@@ -5,7 +5,7 @@ import { LinkIcon } from '@heroicons/react/24/solid'
 
 export default function MenuDocs({title, sectionId, subsectionId}: {title: string, sectionId: string, subsectionId: string}) {
   return (
-    <Link href={`/transparency/${sectionId}/${subsectionId}`} target='_blank' className='w-11/12 h-10 bg-transparent hover:bg-white/30 duration-300 flex flex-row items-center justify-between rounded-lg px-0'>
+    <Link href={`/transparency/${sectionId}/${subsectionId}`} className='w-11/12 h-10 bg-transparent hover:bg-white/30 duration-300 flex flex-row items-center justify-between rounded-lg px-0'>
         <h1 className='text-black font-montserrat text-lg font-normal'>{title}</h1>
         {/* <LinkIcon className='size-6 text-black' /> */}
     </Link>
