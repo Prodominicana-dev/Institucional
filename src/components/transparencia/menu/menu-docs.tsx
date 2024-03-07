d97319d767950359068496b4bcd5afbbbfdb2757
eliampimentel <eliampimentel@prodominicana.gob.do>
2024-03-07 09:24:35 -0400

    a

--- src/components/transparencia/menu/menu-docs.tsx
+++ src/components/transparencia/menu/menu-docs.tsx
@@ -7,7 +7,7 @@ export default function MenuDocs({title, sectionId, subsectionId}: {title: strin
   return (
     <Link href={`/transparency/${sectionId}/${subsectionId}`} className='w-11/12 h-10 bg-transparent hover:bg-white/30 duration-300 flex flex-row items-center justify-between rounded-lg px-0'>
         <h1 className='text-black font-montserrat text-lg font-normal'>{title}</h1>
-        {/* <LinkIcon className='size-6 text-black' /> */}
+        {/* <LinkIcon className='size-6 text-black' /> */} 
     </Link>
   )
 }
