511065f65ee9a6c018238014d0d1e3410baff5e8
RandyFrias <randy140801@gmail.com>
2025-07-02 10:21:18 -0400

    pnfe update

--- src/app/[locale]/(pnfe)/pnfe/page.tsx
+++ src/app/[locale]/(pnfe)/pnfe/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
@@ -13,6 +13,29 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+} from "chart.js";
+import { Doughnut, Bar } from "react-chartjs-2";
+import { fetchTasksData } from "@/services/monday/service";
+import { ScaleLoader } from "react-spinners";
+
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ChartDataLabels
+);
 
 export default function Page() {
   const institutions = [
@@ -189,6 +212,167 @@ export default function Page() {
   ];
   const date = new Date();
   const year = date.getFullYear();
+  const [tasks, setTasks] = useState<any[]>([]);
+  const [generalProgress, setGeneralProgress] = useState(0);
+  const [progressByPilar, setProgressByPilar] = useState<
+    { pilar: string; percentage: number }[]
+  >([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const data = await fetchTasksData();
+      setTasks(data);
+
+      const progress = calculateProgressByPilar(data);
+      const generalProgress = calculateGeneralProgress(data);
+
+      setProgressByPilar(progress);
+      setGeneralProgress(generalProgress);
+
+      console.log("Progreso general:", generalProgress);
+      console.log("Progreso por pilar:", progress);
+    };
+    loadData();
+  }, []);
+
+  function calculateGeneralProgress(data: any[]) {
+    let total = 0;
+    let completed = 0;
+
+    data.forEach((item) => {
+      item.subitems?.forEach((subitem: any) => {
+        total++;
+        const status = subitem.column_values?.find(
+          (col: any) => col.id === "status"
+        )?.text;
+        if (status === "Listo") {
+          completed++;
+        }
+      });
+    });
+
+    if (total === 0) return 0;
+    return Math.round((completed / total) * 100);
+  }
+
+  function calculateProgressByPilar(
+    items: any[]
+  ): { pilar: string; percentage: number }[] {
+    const pilars: Record<string, { total: number; completed: number }> = {};
+
+    items.forEach((item: any) => {
+      const pilarName = item.column_values[0]?.text || "Sin Pilar";
+
+      if (!pilars[pilarName]) {
+        pilars[pilarName] = {
+          total: 0,
+          completed: 0,
+        };
+      }
+
+      item.subitems.forEach((subitem: any) => {
+        pilars[pilarName].total += 1;
+
+        const status = subitem.column_values.find(
+          (cv: any) => cv.id === "status"
+        )?.text;
+        if (status === "Listo") {
+          pilars[pilarName].completed += 1;
+        }
+      });
+    });
+
+    return Object.entries(pilars).map(([pilar, { total, completed }]) => ({
+      pilar,
+      percentage: total === 0 ? 0 : Math.round((completed / total) * 100),
+    }));
+  }
+
+  const gaugeData = {
+    labels: ["Ejecución General", "En gestión"],
+    datasets: [
+      {
+        data: [generalProgress, 100 - generalProgress],
+        backgroundColor: ["#E0193A", "#ffffff"],
+        borderWidth: 0,
+        cutout: "70%",
+        rotation: -90,
+        circumference: 180,
+      },
+    ],
+  };
+
+  const gaugeOptions = {
+    maintainAspectRatio: false,
+    responsive: true,
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: (context: any) => `${context.parsed}%`,
+        },
+      },
+      legend: {
+        position: "bottom" as const,
+        labels: { color: "#fff" },
+      },
+      datalabels: {
+        display: true,
+        color: (context: any) => {
+          const bgColor = context.dataset.backgroundColor[context.dataIndex];
+          return bgColor === "#ffffff" ? "#000000" : "#ffffff";
+        },
+        font: {
+          weight: "bold" as const,
+        },
+        formatter: (value: number, context: any) => {
+          return value > 0 ? `${value}%` : "";
+        },
+      },
+    },
+    cutout: "70%",
+    rotation: -90,
+    circumference: 180,
+  };
+
+  const pilarConfig: Record<string, { label: string; color: string }> = {
+    "PILAR 1.": {
+      label: "Oferta Exportable",
+      color: "#3DA5D9",
+    },
+    "PILAR 2.": {
+      label: "Promoción Internacional",
+      color: "#F17021",
+    },
+    "PILAR 3.": {
+      label: "Facilitación Exportadora",
+      color: "#F2A900",
+    },
+    "PILAR 4.": {
+      label: "Financiamiento Exportador",
+      color: "#CA152B",
+    },
+    "PILAR 5.": {
+      label: "Transversal Institucionalidad",
+      color: "#8E44AD",
+    },
+  };
+
+  const findPilarConfig = (name: string) => {
+    const entry = Object.entries(pilarConfig).find(([key]) =>
+      name.includes(key)
+    );
+    return entry?.[1] || { label: name, color: "#CCCCCC" };
+  };
+
+  const bars = progressByPilar.map(({ pilar, percentage }) => {
+    const config = findPilarConfig(pilar);
+    return {
+      label: config.label,
+      value: percentage,
+      color: config.color,
+    };
+  });
+
   return (
     <div className="bg-white overflow-hidden">
       <div className="w-full flex flex-col xl:flex-row items-center justify-center xl:justify-between h-8 bg-[#024C97] text-xs text-white px-2 xl:px-28">
@@ -250,7 +434,9 @@ export default function Page() {
                 Casos de Éxito
               </Link>
               <Link
-                href="#documents"
+                href="/documents/pnfe.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="hover:text-sky-700 duration-300"
               >
                 Documentos
@@ -342,9 +528,14 @@ export default function Page() {
               inicia con levantamientos desde un proceso de trabajo desde 2016,
               se entrega un primer documento en el 2018 y no es hasta el pasado
             </p>
-            <button className="font-semibold text-red-600 hover:underline hover:text-red-700 hover:cursor-pointer duration-200">
+            <Link
+              href="/documents/pnfe.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-semibold text-red-600 hover:underline hover:text-red-700 hover:cursor-pointer duration-200"
+            >
               Leer más...
-            </button>
+            </Link>
             <div className="hidden xl:block">
               <div className="absolute top-0 right-0 w-full h-full bg-[#CA152B] [clip-path:polygon(65%_0%,100%_0%,100%_20%,58.2%_20%)]" />
               <div className="absolute bottom-0 left-0 w-full h-full bg-[#003B7A] [clip-path:polygon(0%_80%,33%_80%,25%_100%,0%_100%)]" />
@@ -353,17 +544,51 @@ export default function Page() {
         </div>
       </section>
       <section
-        id="sectors"
+        id="progress"
+        className="bg-[#003B7A] xl:h-screen w-full py-5 xl:py-20 px-5 xl:px-40"
+      >
+        <h1 className="text-2xl xl:text-5xl font-bold text-white uppercase mb-5">
+          Avances del Plan
+        </h1>
+        {bars.length === 0 ? (
+          <div className="flex justify-center items-center h-full">
+            <ScaleLoader width={10} height={25} color="white" />
+          </div>
+        ) : (
+          <div className="flex flex-col xl:flex-row justify-center items-center gap-5 xl:gap-20 h-5/6">
+            <div className="h-full xl:w-6/12">
+              <Doughnut data={gaugeData} options={gaugeOptions} />
+            </div>
+            <div className="h-full xl:w-6/12">
+              {bars.map((bar, index) => (
+                <BarChart key={index} data={bar} />
+              ))}
+            </div>
+          </div>
+        )}
+      </section>
+      <section
+        id="milestones"
+        className="xl:h-screen bg-white w-full py-5 xl:py-20 px-5 xl:px-28 relative"
+      >
+        <h1 className="text-2xl xl:text-3xl font-bold text-[#003B7A] uppercase mb-5">
+          Hitos
+        </h1>
+        <div className="hidden xl:block">
+          <div className="absolute top-0 right-0 w-full h-full bg-[#003B7A] [clip-path:polygon(65%_0%,100%_0%,100%_20%,58.2%_20%)]" />
+        </div>
+      </section>
+      <section
+        id="institutions"
         className="flex flex-col bg-white w-full gap-10 py-5 xl:py-20 px-5 xl:px-28"
       >
         <h1 className="text-2xl xl:text-3xl font-bold text-[#003B7A] uppercase">
-          Sectores Público y Privado
+          Instituciones Públicas y Privadas
         </h1>
         <Carousel
           opts={{
             align: "start",
             loop: true,
-            slidesToScroll: 4,
           }}
           plugins={[
             Autoplay({
@@ -446,6 +671,87 @@ export default function Page() {
           </div>
         </div>
       </section>
+    </div>
+  );
+}
+
+function BarChart({ data }: any) {
+  const barData = {
+    labels: [data.label],
+    datasets: [
+      {
+        label: "Avance",
+        data: [data.value],
+        backgroundColor: [data.color],
+      },
+      {
+        label: "En gestión",
+        data: [100 - data.value],
+        backgroundColor: "#ffffff",
+      },
+    ],
+  };
+
+  const barOptions = {
+    maintainAspectRatio: false,
+    indexAxis: "y" as const,
+    responsive: true,
+    scales: {
+      x: {
+        stacked: true,
+        display: false,
+        grid: { display: false },
+      },
+      y: {
+        stacked: true,
+        display: false,
+        grid: { display: false },
+      },
+    },
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: (context: any) =>
+            `${context.dataset.label}: ${context.parsed.x}%`,
+        },
+      },
+      legend: {
+        position: "bottom" as const,
+        align: "center" as const,
+        labels: { color: "#fff" },
+      },
+      datalabels: {
+        display: true,
+        color: (context: any) => {
+          const datasetIndex = context.datasetIndex;
+          const dataset = context.chart.data.datasets[datasetIndex];
+          const bgColor = dataset.backgroundColor;
+          if (typeof bgColor === "string") {
+            return bgColor === "#ffffff" ? "#000000" : "#ffffff";
+          }
+
+          if (Array.isArray(bgColor)) {
+            const value = bgColor[context.dataIndex];
+            return value === "#ffffff" ? "#000000" : "#ffffff";
+          }
+
+          return "#000000";
+        },
+        font: {
+          weight: "bold" as const,
+        },
+        formatter: (value: number) => `${value}%`,
+      },
+    },
+  };
+  return (
+    <div>
+      <h2 className="xl:text-xl font-bold uppercase text-white">
+        {data.label}
+      </h2>
+      <div className="h-32">
+        <Bar data={barData} options={barOptions} />
+      </div>
     </div>
   );
 }
